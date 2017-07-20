8ea4be8c1d6a74a8f98c013bf60d256c51d78b72
Ditchbuster <trioman008@hotmail.com>
2017-07-20 10:10:29 -0600

    removal of despawned resources
    Added in functionality to remove despawned resources
    gets the xml that GH produces every morning of all active resources and
    compares to the server output xml

--- gh_push_public.py
+++ gh_push_public.py
@@ -6,6 +6,7 @@ user = #YOUR USER HERE
 passu = #YOUR PASSWORD HERE
 galaxy = #GALAXY ID NUMBER FOR YOUR SERVER
 planets = ['corellia','dantooine','dathomir','endor','lok','naboo','rori','talus','tatooine','yavin4']
+serverFilename = '/home/vagrant/workspace/Core3/MMOCoreORB/bin/scripts/managers/ghoutput.xml'
 global token
 
 def login():
@@ -29,50 +30,50 @@ def checkSpawn(name):
 		return False
 
 def readSpawnOutput(token):
-	tree = ET.parse('/home/vagrant/workspace/Core3/MMOCoreORB/bin/scripts/managers/ghoutput.xml')
+	tree = ET.parse(serverFilename)
 	root = tree.getroot()
 	for resource in root.findall('resource'):
-			name = resource.find('SpawnName').text
-			res = resource.find('resType').text
-			planet = resource.find('planet').text
-			#print name + " " + res + " " + planet
-			CR = ''
-			CD = ''
-			DR = ''
-			FL = ''
-			HR = ''
-			MA = ''
-			PE = ''
-			OQ = ''
-			SR = ''
-			UT = ''
-			ER = ''
-			for attribute in resource.findall('attribute'):
-				attname = attribute.get('name')
-				stat = attribute.text
-				if attname == 'res_cold_resist':
-					CR = stat
-				elif attname == 'res_conductivity':
-					CD = stat
-				elif attname == 'res_decay_resist':
-					DR = stat
-				elif attname == 'res_flavor':
-					FL = stat
-				elif attname == 'res_heat_resist':
-					HR = stat
-				elif attname == 'res_malleability':
-					MA = stat
-				elif attname == 'res_potential_energy':
-					PE = stat
-				elif attname == 'res_quality':
-					OQ = stat
-				elif attname == 'res_shock_resistance':
-					SR = stat
-				elif attname == 'res_toughness':
-					UT = stat
-				elif attname == 'entangle_resistance':
-					ER = stat
-			postSpawn(token, name, res, planet, CR, CD, DR, FL, HR, MA, PE, OQ, SR, UT, ER)
+		name = resource.find('SpawnName').text
+		res = resource.find('resType').text
+		planet = resource.find('planet').text
+		#print name + " " + res + " " + planet
+		CR = ''
+		CD = ''
+		DR = ''
+		FL = ''
+		HR = ''
+		MA = ''
+		PE = ''
+		OQ = ''
+		SR = ''
+		UT = ''
+		ER = ''
+		for attribute in resource.findall('attribute'):
+			attname = attribute.get('name')
+			stat = attribute.text
+			if attname == 'res_cold_resist':
+				CR = stat
+			elif attname == 'res_conductivity':
+				CD = stat
+			elif attname == 'res_decay_resist':
+				DR = stat
+			elif attname == 'res_flavor':
+				FL = stat
+			elif attname == 'res_heat_resist':
+				HR = stat
+			elif attname == 'res_malleability':
+				MA = stat
+			elif attname == 'res_potential_energy':
+				PE = stat
+			elif attname == 'res_quality':
+				OQ = stat
+			elif attname == 'res_shock_resistance':
+				SR = stat
+			elif attname == 'res_toughness':
+				UT = stat
+			elif attname == 'entangle_resistance':
+				ER = stat
+		postSpawn(token, name, res, planet, CR, CD, DR, FL, HR, MA, PE, OQ, SR, UT, ER)
 
 def postSpawn(token, name, res, planet, CR, CD, DR, FL, HR, MA, PE, OQ, SR, UT, ER):
 	if planet == 'yavin4':
@@ -90,6 +91,47 @@ def postSpawn(token, name, res, planet, CR, CD, DR, FL, HR, MA, PE, OQ, SR, UT, 
 	result = xmldoc.find('resultText').text
 	print result
 
+def removeDespawned(token, gal = galaxy):
+    #print(len(ghNames),len(serverNames),len(ghNames)-len(serverNames),len(resources))
+    for res in resources:
+        #print(token,res,gal)
+        url = url = 'http://galaxyharvester.net/markUnavailable.py?galaxy=' + gal + '&spawn=' + res + '&planets=all&gh_sid=' + token
+        response = urllib2.urlopen(url)
+        resp = response.read().decode('utf-8')
+        
+        
+    if filename == None:
+        url = 'http://galaxyharvester.net/exports/current' + gal + '.xml'
+        response = urllib2.urlopen(url)
+        xml = response.read()
+        root = ET.fromstring(xml)
+        
+    else:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+     #TODO: check for errors
+    
+    if root.tag != "resources":
+        print("Is this the expected file from GH?")
+    names = root.findall(".//resource/name")
+    unqNames = set()
+    for n in names:
+        unqNames.add(n.text)
+    return unqNames
+   
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    if root.tag != "SpawnOutput":
+        print("Is this the expected file from server?")
+    #return(root.xpath("//resource/SpawnName/text())) #we want unique names
+    serverNames = set()
+    for res in root.iter("resource"):
+        for child in res:
+            if child.tag == "SpawnName":
+                serverNames.add(str.lower(child.text))
+    return serverNames
+
 token = login()
+removeDespawned(token)
 readSpawnOutput(token)
 
